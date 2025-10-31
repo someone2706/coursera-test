b16f3b503bc810671711fafa0dab7af18d316a82
Nyasha singh <nyashasingh.27@gmail.com>
2025-10-31 17:19:48 +0530

    Refactor HTML snippet paths in script.js
    Updated HTML snippet paths to remove 'snippets/' prefix.

--- module5-sol/script.js
+++ module5-sol/script.js
@@ -357,15 +357,15 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "snippets/home-snippet.html";
+var homeHtmlUrl = "home-snippet.html";
 var allCategoriesUrl =
   "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
-var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-var categoryHtml = "snippets/category-snippet.html";
+var categoriesTitleHtml = "categories-title-snippet.html";
+var categoryHtml = "category-snippet.html";
 var menuItemsUrl =
   "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
-var menuItemsTitleHtml = "snippets/menu-items-title.html";
-var menuItemHtml = "snippets/menu-item.html";
+var menuItemsTitleHtml = "menu-items-title.html";
+var menuItemHtml = "menu-item.html";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
